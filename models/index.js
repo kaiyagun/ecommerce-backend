c3aa5b59d50d31109c15bfd77dc056f4f1da62e2
kaiya gunsauls <kaiyagunsauls@gmail.com>
2021-10-28 14:59:24 -0700

    get request for all categories, figured out seeding and deployed to heroku

--- models/index.js
+++ models/index.js
@@ -4,21 +4,19 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
-// Categories have many Products
+Category.hasMany(Product)
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  // as: 'product_tags',
+  as: 'product_tags',
   foreignKey: 'product_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  // as: 'product_tags',
+  as: 'product_tags',
   foreignKey: 'tag_id',
 });
 
